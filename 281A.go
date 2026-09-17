2a9d8a5143c6665bb8bd5b73f63d10ea28f3b920
Egor Chuprov <ytdrenk@gmail.com>
2026-09-17 15:52:03 +0300

    281

---
+++ 281A.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	r := bufio.NewReaderSize(os.Stdin, 1<<20)
+	w := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer w.Flush()
+
+	line, _ := r.ReadString('\n')
+	line = strings.TrimSpace(line)
+
+	runes := []rune(line)
+	runes[0] = unicode.ToUpper(runes[0])
+	w.WriteString(string(runes))
+	w.WriteByte('\n')
+}
---
+++ 791A.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+	r := bufio.NewReaderSize(os.Stdin, 1<<20)
+	w := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer w.Flush()
+
+	line, _ := r.ReadString('\n')
+	line = strings.TrimSpace(line)
+}
